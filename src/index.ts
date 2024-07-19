e933f467b69a342ec2b6ff2dffdb47fea4ba0065
Sumit Nair <sumit.nair26@gmail.com>
2024-07-19 15:40:21 +0530

    KAFKA implementation with NODEJS
    using kafkajs NPM Package

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:9092']
+})
+
+const producer = kafka.producer()
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
+const run = async () => {
+  // Producing
+  await producer.connect()
+  await producer.send({
+    topic: 'quickstart-events',
+    messages: [
+      { value: 'Hello KafkaJS user from NodeJS process' },
+    ],
+  })
+
+  // Consuming
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'quickstart-events', fromBeginning: true })
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+        if (!message.value) {
+            return;
+        }
+      console.log({
+        partition,
+        offset: message.offset,
+        value: message.value.toString(),
+      })
+    },
+  })
+}
+
+run().catch(console.error)
