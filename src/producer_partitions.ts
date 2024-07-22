c3c9f36c20d7a609eb96ce334fdc9ec9d697b9d8
Sumit Nair <sumit.nair26@gmail.com>
2024-07-22 14:45:45 +0530

    Partitions
    Consumer and Producer Partitions

---
+++ src/producer_partitions.ts
@@ -0,0 +1,21 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:9092']
+})
+
+const producer = kafka.producer()
+
+const run = async () => {
+  // Producing
+  await producer.connect()
+  await producer.send({
+    topic: 'payment-done',
+    messages: [
+      { value: 'Hello KafkaJS user from Payment Done' },
+    ],
+  })
+}
+
+run().catch(console.error)
